5eddd44a25715e02ecd66720697accc05106c83b
harithamadi <harithamadisetty@gmail.com>
2020-06-06 20:26:48 -0500

    added Verify Reset Button Verify International - Documents Verify International - Country of Origin

--- src/test/java/com/generic/functional/automation/ui/tests/home/HomePageAutomation.java
+++ src/test/java/com/generic/functional/automation/ui/tests/home/HomePageAutomation.java
@@ -18,14 +18,14 @@ public class HomePageAutomation extends TestConfig {
         helpButton.click();
         test.log(Status.INFO, "Help Button Clicked");
         driver.findElement(By.id("mainArc-0b981a1b-32dc-43b1-b257-70c8c5a6cc6d")).click();
-        Thread.sleep(1*1000);
+        Thread.sleep(5*1000);
         test.log(Status.INFO, "Shipments cluster found");
         driver.findElement(By.id("mainArc-d5f6e57b-d157-4ec5-bbc4-9fd35cc68775")).click();
-        Thread.sleep(1*1000);
+        Thread.sleep(5*1000);
         test.log(Status.INFO, "Service cluster found");
         driver.findElement(By.id("mainArc-f18c2bb1-ad7d-4583-8b64-7ef98ba647cc")).click();
-        Thread.sleep(2*1000);
-        driver.findElement(By.xpath("//*[text()='Total Records: ']"));
+        Thread.sleep(5*1000);
+        driver.findElement(By.xpath("//*[text()=' Total Records']"));
         //driver.findElement(By.xpath("//*[text()='28']"));
 
         test.createNode("Total Records found means table loaded");
@@ -36,48 +36,49 @@ public class HomePageAutomation extends TestConfig {
 
 
     @Test
-    public void testHelpButtonIntenationalClick() throws Exception {
-        test = extent.createTest("Help Button International Click");
+    public void testVerifyInternationalCountryOfOrigin() throws Exception {
+        test = extent.createTest("Verify International Facts Country Of Origin");
         login.doLogin(test);
-        test.log(Status.INFO, "Looking for help button");
+        test.log(Status.INFO, "Help button Clicked");
         WebElement helpButton = driver.findElement(By.cssSelector(".explore-quiries-inner"));
         helpButton.click();
-        test.log(Status.INFO, "Help Button Clicked");
-        test.log(Status.INFO, "Shipments link Clicked on the Sunburst");
+        test.log(Status.INFO, "Sunburst Shown");
+        test.log(Status.INFO, "Shipments Cluster Clicked on the Sunburst");
         driver.findElement(By.id("mainArc-0b981a1b-32dc-43b1-b257-70c8c5a6cc6d")).click();
-        Thread.sleep(1*1000);
-        test.log(Status.INFO, "International link Clicked on the Sunburst");
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "International Clicked on the Sunburst");
         driver.findElement(By.id("mainArc-71ef3c15-be01-454d-bd1e-c59d13904a65")).click();
-        Thread.sleep(2*1000);
-        test.log(Status.INFO, "Country of Origin link Clicked on the Sunburst");
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Country of Origin link Clicked on the Sunburst");
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "Country of Origin Element Clicked on the Sunburst");
         WebElement helpButtoncountry = driver.findElement(By.id("mainArc-466ed1b9-526b-45c7-a02c-e6d419ef606f"));
         helpButtoncountry.click();
-
-        Thread.sleep(2*1000);
-        driver.findElement(By.xpath("//*[text()='Total Records: ']"));
-        test.createNode("Total Records found means table loaded");
+        Thread.sleep(5*1000);
+        driver.findElement(By.xpath("//*[text()=' Total Records']"));
+        test.log(Status.INFO,"Total Records found and table shown");
 
     }
 
     @Test
-    public void testShipmentsDocumentsClick() throws Exception {
-        test = extent.createTest("Shipments Documents Click");
+    public void testVerifyInternationalDocumentsClick() throws Exception {
+        test = extent.createTest("Verify International Documents");
         login.doLogin(test);
-        test.log(Status.INFO, "Looking for help button");
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "Help Button Clicked");
         WebElement helpButton = driver.findElement(By.cssSelector(".explore-quiries-inner"));
         helpButton.click();
-        test.log(Status.INFO, "Help Button Clicked");
-
-
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "Sunburst Shown");
         driver.findElement(By.id("mainArc-0b981a1b-32dc-43b1-b257-70c8c5a6cc6d")).click();
-        Thread.sleep(1*1000);
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "Shipments Cluster Clicked");
         driver.findElement(By.id("mainArc-71ef3c15-be01-454d-bd1e-c59d13904a65")).click();
-        Thread.sleep(1*1000);
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "International Clicked on Sunburst");
         driver.findElement(By.id("mainArc-044c1070-d406-443b-a9e5-2fdbdf4daaae")).click();
-        Thread.sleep(1*1000);
-
-        driver.findElement(By.xpath("//*[text()='Total Records: ']"));
+        Thread.sleep(5*1000);
+        test.log(Status.INFO, "Documents Element Clicked");
+        driver.findElement(By.xpath("//*[text()=' Total Records']"));
+        test.log(Status.INFO,"Total Records found and table shown");
 
     }
 
@@ -106,52 +107,78 @@ public class HomePageAutomation extends TestConfig {
 
     @Test
     public void testResetButton() throws Exception {
-        test = extent.createTest("Reset Button Test");
+        test = extent.createTest("Verify Reset Button");
         login.doLogin(test);
 
-        test.log(Status.INFO, "Testing the Functionality of the Reset button");
-
-      //  driver.get("https://a4data-qe.netlify.app/dashboard");
+        test.log(Status.INFO,"Help Button Clicked");
+        WebElement helpButton = driver.findElement(By.cssSelector(".explore-quiries-inner"));
+        helpButton.click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Sunburst Shown");
+        driver.findElement(By.id("mainArc-0b981a1b-32dc-43b1-b257-70c8c5a6cc6d")).click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Clicked on Shipments Cluster");
+        driver.findElement(By.id("mainArc-71ef3c15-be01-454d-bd1e-c59d13904a65")).click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Clicked on International");
+        driver.findElement(By.id("mainArc-7000c84e-b726-405b-ae57-3103e755a869")).click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Clicked on Sub element to get the Query");
+        driver.findElement(By.id("mainArc-a4b796c9-8bbb-4f37-b959-26b2e1dd758e")).click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Clicked on Sub element to get the Query");
+        driver.findElement(By.id("mainArc-dcf61afd-31e9-4faa-9e8f-8743a267ac26")).click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Clicked on Sub element to get the Query");
+        driver.findElement(By.cssSelector("svg.MuiSvgIcon-root.highlightIcon")).click();
+        Thread.sleep(5 * 1000);
         driver.findElement(By.xpath("//input[@name='']")).click();
+        Thread.sleep(5 * 1000);
+        test.log(Status.INFO, "Clicked on Reset Button");
         driver.findElement(By.xpath("//input[@name='']")).clear();
-
-
-
+        Thread.sleep(3 * 1000);
         driver.findElement(By.xpath("//input[@name='']")).sendKeys("list all international");
-        test.log(Status.INFO, "Entered Query No.1");
-        Thread.sleep(1*1000);
+        test.log(Status.INFO, "Entered a Query in search bubble");
+        Thread.sleep(3*1000);
         driver.findElement(By.cssSelector("svg.MuiSvgIcon-root.highlightIcon > path")).click();
         test.log(Status.INFO, "Reset Button Clicked");
-        Thread.sleep(1*1000);
-        driver.findElement(By.xpath("//input[@name='']")).sendKeys("list all customers by city");
+        Thread.sleep(2*1000);
+     /*   driver.findElement(By.xpath("//input[@name='']")).sendKeys("list all customers by city");
         test.log(Status.INFO, "Entered Query No.2");
-        Thread.sleep(1*1000);
+        Thread.sleep(5*1000);
         driver.findElement(By.cssSelector("svg.MuiSvgIcon-root.highlightIcon > path")).click();
-        test.log(Status.INFO, "Reset Button Clicked");
+        test.log(Status.INFO, "Reset Button Clicked");*/
 
     }
     @Test
     public void testVerifySubjectGuides() throws Exception {
         test = extent.createTest("Verify the Subject Guide Test");
-        logicAndClickHelpButton();
+        login.doLogin(test);
+        test.log(Status.INFO,"Help Button Clicked");
         WebElement helpButton = driver.findElement(By.cssSelector(".explore-quiries-inner"));
         helpButton.click();
-        test.log(Status.INFO, "Testing the Functionality of the Verify the Subjects Guide Label");
-        //driver.get("https://a4data-qe.netlify.app/dashboard/");
+        Thread.sleep(5 * 1000);
         driver.findElement(By.id("mainArc-890e16a2-6415-4661-982d-33acafbb1fed")).click();
-        Thread.sleep(1*1000);
+        Thread.sleep(5 * 1000);
         driver.findElement(By.id("mainArc-656a2ba3-4af2-471a-a5ef-6b3decb8dfeb")).click();
-        Thread.sleep(1*1000);
+        Thread.sleep(5 * 1000);
         driver.findElement(By.id("mainArc-df1ff2c9-14ac-4aa1-8572-93a4cd914093")).click();
-        Thread.sleep(1*1000);
-        driver.findElement(By.id("mainArc-2ba8d1f2-779e-4574-b83e-046a283eff931")).click();
-        Thread.sleep(1*1000);
-        WebElement element = driver.findElement(By.xpath("//*[text()='Domain of Values']"));
-        System.out.println("$$$$ Check the element" +element);
-        test.log(Status.INFO, "Found Domain Domain of Values");
-        Thread.sleep(2*1000);
-        driver.findElement(By.xpath("//*[text()='']"));
-        test.log(Status.INFO, "Found Domain Drill down for Sub-elements");
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.linkText("Customer")).click();
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.id("mainArc-dde43c3d-c500-407e-9786-7fb0b1d39b85")).click();
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.linkText("Customer")).click();
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.id("mainArc-cf0ac243-723d-468b-b40c-91195565e4e0")).click();
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.linkText("Customer")).click();
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.id("mainArc-79796cc3-bd59-437c-9763-495a5f1fb6bc")).click();
+        Thread.sleep(5 * 1000);
+        driver.findElement(By.id("mainArc-79796cc3-bd59-437c-9763-495a5f1fb6bc1")).click();
+        Thread.sleep(5 * 1000);
+
     }
 
     public void logicAndClickHelpButton() throws Exception {
