f2117289d3ae1c3d94b9fed313f4fd0906aa7fba
Gopi <geetapillutla123@gmail.com>
2020-06-17 14:04:42 -0500

    1st validation

--- src/test/java/com/generic/functional/automation/ui/tests/login/InvalidCredentialsTest.java
+++ src/test/java/com/generic/functional/automation/ui/tests/login/InvalidCredentialsTest.java
@@ -11,31 +11,23 @@ import java.util.concurrent.TimeUnit;
 public class InvalidCredentialsTest extends TestConfig {
     @Test
     public void invalidUserNameAndPasswordTest() throws Exception {
-        test = extent.createTest("InvalidCredentialsTest",
-                "Checking Invalid Credentials");
-
+        test = extent.createTest("InvalidCredentialsTest");
         test.createNode("Typing User Name");
         WebElement userName = driver.findElement(By.id("email"));
         userName.click();
         userName.clear();
-
-        Thread.sleep(1*1000);
+        Thread.sleep(5*1000);
         userName.sendKeys("geeta");
-
-        Thread.sleep(1*1000);
-
+        Thread.sleep(5*1000);
         test.createNode("Typing Password");
         WebElement password = driver.findElement(By.id("password"));
         password.click();
         password.clear();
         password.sendKeys("Design_20");
-
         WebElement span = driver.findElement(By.xpath("//div[@id='gatsby-focus-wrapper']/div/main/div/main/div/form/button/span"));
         span.click();
-
         Thread.sleep(10*1000);
         boolean b = driver.getPageSource().contains("Username or password doesn't exists.");
-
         String s = "Username or password doesn't exists.";
         if (!b) {
             s = "";
