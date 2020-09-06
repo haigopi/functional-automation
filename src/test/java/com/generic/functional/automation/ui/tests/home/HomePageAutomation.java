0de2181ef61c52ff4651a5d2b89467909f0f66e1
haritha <harithamadisetty@gmail.com>
2020-09-06 16:25:31 -0500

    ids,classnames and nodes added.

--- src/test/java/com/generic/functional/automation/ui/tests/home/HomePageAutomation.java
+++ src/test/java/com/generic/functional/automation/ui/tests/home/HomePageAutomation.java
@@ -361,7 +361,7 @@ public class HomePageAutomation extends TestConfig {
 
     @Test
 
-    public void testVerifyResetButtonSunburst() throws Exception { //*** Haritha
+    public void testVerifyResetButtonSunburst() throws Exception { //** Haritha
         test = extent.createTest("Home Page Verify Reset Button by Sunburst");
         driver.manage().timeouts().implicitlyWait(time_to_wait, TimeUnit.SECONDS);//this is global so no need to mention multiple times
         try {
